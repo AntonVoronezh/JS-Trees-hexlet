745ba9919bf0db44a6c84ae7a8a1dcaca7b5c097
AntonVoronezh <pstut@mail.ru>
2018-11-26 19:16:08 +0300

    5 #5

---
+++ 5/task.js
@@ -0,0 +1,21 @@
+const findFilesByName = (argTree, argStr) => {
+  const clonedObj = { ...argTree };
+  const pathArr = [];
+
+  const dfs = (argTree, acc) => {
+    const newAcc = acc.concat(argTree.name);
+    if (argTree.type === 'directory') {
+      return { ...argTree, children: argTree.children.map(elem => dfs(elem, newAcc)) };
+    }
+    if (argTree.name.includes(argStr)) {
+      const [first, ...rest] = newAcc;
+      // console.log('acc -> '+ '/' + rest.join('/'));
+      pathArr.push('/' + rest.join('/'));
+    }
+    return { ...argTree };
+  };
+  dfs(clonedObj, []);
+  return pathArr;
+};
+
+export default findFilesByName;
